68650dbb9a13f167c4b1f3c65b8c1bacbcead35d
zhangge <1252496768@qq.com>
2025-07-18 22:34:25 +0800

    can基础通信正常

--- src/main.c
+++ src/main.c
@@ -4,15 +4,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
-
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS 1
-
+#define LED0_NODE     DT_ALIAS(led0)
+const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+extern int can_init(void);
 int main(void)
 {
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE); /* 先点亮 */
+	can_init();
 	while (1) {
-		k_msleep(1000);
+		gpio_pin_toggle_dt(&led);
+		k_msleep(500);
 	}
 	return 0;
 }
