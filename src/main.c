81c69eb9f41d55084645dac7f07a1f1ac6c52a62
zhangge <1252496768@qq.com>
2025-09-06 03:21:06 +0800

    适配motorlib调试AS5047,调试中

--- src/main.c
+++ src/main.c
@@ -11,13 +11,17 @@
 #define LED0_NODE     DT_ALIAS(led0)
 const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 // extern int can_init(void);
+extern void *as5047_readangle(void);
+
 int main(void)
 {
 	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE); /* 先点亮 */
 	printk("hello world! %s\n", CONFIG_BOARD);
+	k_msleep(500);
 	while (1) {
 		gpio_pin_toggle_dt(&led);
-		k_msleep(50);
+		as5047_readangle();
+		k_msleep(500);
 	}
 	return 0;
 }
